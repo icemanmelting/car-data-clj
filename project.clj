f774ba8496e33a5926c8b53ef150e431ce329c09
iCeMan <fabiodiasfrancisco27@gmail.com>
2017-05-30 18:31:15 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject car-data-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.postgresql/postgresql "42.1.1"]
+                 [com.layerware/hugsql "0.4.5"]])
