fdebda4fe010a2ebde1410e4e368e67bb14a4a03
iCeMan <fabiodiasfrancisco27@gmail.com>
2018-09-01 15:36:41 +0100

    fixed bug with car retrieval

---
+++ resources/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (login, password, salt) VALUES ('fabio@kadett.pt', '3bb95188c01763e81875ce9644f496a4e3f98d9eb181c5f128ba32a01b62b6de', 'bar');
+
+INSERT INTO cars (id, owner, dashboard_type, constant_kilometers, trip_kilometers, tyre_offset) VALUES ('4fff4f67-1c85-4a62-9288-01f63dda9907', 'fabio@kadett.pt', 'kadett', 28792.506618081927, 77.6291693045656, 1);
